c5f08fedd8e5f390a0123a7430c1904ef95aaf45
Anmol Dhar <anmoldhar4@gmail.com>
2023-11-10 14:20:17 +0530

    Adapter class created and minor changes.

---
+++ app/src/main/java/com/anmol/easypdfreader/Adapter.java
@@ -0,0 +1,73 @@
+package com.anmol.easypdfreader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ShareCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    Activity context;
+    List<File> list;
+
+    public Adapter(Activity context, List<File> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.rv_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+        File file = list.get(position);
+        holder.name.setText(file.getName());
+        holder.path.setText(file.getPath());
+
+        holder.share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = ShareCompat.IntentBuilder.from(context).setText("application/pdf")
+                        .setStream(Uri.parse(file.getPath()))
+                        .setChooserTitle("Choose app")
+                        .createChooserIntent()
+                        .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView name, path;
+        ImageView share;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.file_name);
+            path = itemView.findViewById(R.id.file_path);
+            share = itemView.findViewById(R.id.share_btn);
+
+        }
+    }
+}
